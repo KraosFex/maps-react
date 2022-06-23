82f12991c50d52213fc6300430516ab91eabaf8c
KraosFex <ferreirajohan14253@gmail.com>
2022-06-22 22:41:49 -0400

    feat(src/context/*) the interfaces for the initial states of the provider and the context were created. in addition to general the reducer

--- src/context/place/ContexProvider.ts
+++ src/context/place/ContexProvider.ts
@@ -1,4 +1,9 @@
+// este es el conteto que se ejecura segun lo que este ocurriendo
 import { createContext } from "react";
 
+export interface PlacesContexProps {
+    isLoading: boolean;
+    userLocation?: [number, number]
+}
 
-export const PlecesContext = createContext({});
+export const PlecesContext = createContext<PlacesContexProps>({} as PlacesContexProps);
--- src/context/place/PlacesProvider.tsx
+++ src/context/place/PlacesProvider.tsx
@@ -1,3 +1,15 @@
+
+export interface PlacesState {
+    isLoading: boolean;
+    userLocation?: [number, number]
+}
+
+// este es el inicial state para toda nuesta app
+const INITIAL_STATES: PlacesState = {
+    isLoading: true,
+    userLocation: undefined,
+}
+
 export const PlacesProvider = () => {
     return(
         <>
